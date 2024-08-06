7590f973bded5ba4ae1781dcea92aa6d08d5b2f6
pangelu9 <pangeliki27@gmail.com>
2024-08-06 13:55:31 +0300

    clean up files

--- filter_packages.py
+++
@@ -1,7 +0,0 @@
-input_file = 'packages_list.txt'
-output_file = 'filtered_packages_list.txt'
-
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
-        if 'pypi' not in line:
-            outfile.write(line)
